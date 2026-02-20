888b31681fbb4dc195c9533e101dcda4fe154bb1
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 16:25:09 -0500

    Update ReasonSkills.py

--- ReasonSkills.py
+++ ReasonSkills.py
@@ -1,1 +1,87 @@
-
+class ReasonSkills:
+    S_plus_skills = (Sage 
+Distortion Reaper
+Psychology + 5
+Manifestation + 5
+Self Belief + 5
+Affirmation Strength Lvl 3
+Brainwash Resistance + 5
+Red Pill + 5
+)
+    S_skills = (Distortion Destroyer
+Stoic Philosopher 
+Atomic Habiter
+Scarcity Breaker
+Oneitis Cure
+Flaw Breaker + 3
+Self Belief +  4
+Abundance Mindset + 5
+Affirmation Strength Lvl 2
+Brainwash Resistance + 4
+)
+    A_plus_skills = (Habit Grandmaster
+Auditory Manifestation
+Stoicism + 3
+Self Awareness + 3
+Psychology + 4
+Manifestation + 4
+Abundance Mindset + 4
+Brainwash Resistance + 3
+Red Pill + 4
+Reason Prowess Lvl. 5
+)
+    A_skills = (Distortion Slayer
+Monk 
+Stoic 
+Visual Manifestation
+Healthy Habits + 3
+Self Belief + 3
+Affirmation Strength Lvl 1
+)
+    B_plus_skills = (Protein Master
+Habit Scholar
+Manifestation
+Healthy Sleep Schedule
+Self Improver
+Healthy Habits + 2
+Self Awareness + 2
+Flaw Breaker + 2
+Psychology + 3
+Manifestation + 3
+Abundance Mindset + 3
+Reason Prowess Lvl. 4 
+)
+    B_skills = (Healthy Eater
+Stoicism + 2
+Flaw Breaker + 1
+Self Awareness + 1
+Brainwash Resistance + 2
+Red Pill + 3
+)
+    C_plus_skills = (Habit Apprentice
+Healthy Habits + 1
+Stoicism + 1
+Novice Stoic 
+Self Belief + 2
+Red Pill + 2
+Reason Prowess Lvl. 3 
+)
+    C_skills = (Bed Maker
+Meditate
+Abundance Mindset + 2
+Psychology + 2
+Manifestation + 2
+Brainwash Resistance + 1
+)
+    D_plus_skills = (Journal 
+Protein Drinker 
+Self Belief + 1
+Red Pill + 1
+Reason Prowess Lvl. 2
+)
+    D_skills = (Abundance Mindset + 1)
+    
+    E_plus_skills = (Reason Prowess Lvl. 1
+Psychology + 1
+Manifestation + 1
+)
