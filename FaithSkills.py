21b66305009b718654219e5e998ef0fb6a37e592
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 16:27:04 -0500

    Update FaithSkills.py

--- FaithSkills.py
+++ FaithSkills.py
@@ -1,1 +1,56 @@
-
+class FaithSkills:
+    S_plus_skills = (Child of God
+Luck + 5
+Risk Taking + 5
+Bible Knowledge + 5
+)
+    S_skills = (Divine Knowledge
+Sacred Power
+Divine Fortune
+Miracle Granter
+Luck + 4
+Risk Taking + 4
+Karma + 5
+Bible Reading Lvl 3
+Bible Knowledge + 4
+)
+    A_plus_skills = (Pro-Life Gambler
+Scripture Reader
+Karma + 4
+Faith Prowess Lvl. 5
+)
+    A_skills = (Divine Bond + 3
+Humility + 3
+Luck + 3
+Risk Taking + 3
+Bible Knowledge + 3
+)
+    B_plus_skills = (Sacred Prayer
+Virtue + 3
+Faith Prowess Lvl. 4
+)
+    B_skills = (Divine Bond + 2 
+Humility + 2
+Virtue + 2
+Risk taking + 2
+Karma + 3
+Bible Reading Lvl 2
+Bible Knowledge + 2
+)
+    C_plus_skills = (Faith Prowess Lvl. 3 
+Divine Bond + 1
+Luck + 2
+)
+    C_skills = (Virtue + 1
+Humility + 1
+Risk Taking + 1
+Bible Knowledge + 1
+)
+    D_plus_skills = (Luck + 1
+Karma + 2
+Bible Reading Lvl 1
+Faith Prowess Lvl. 2
+)
+    D_skills = (Karma + 1)
+    E_plus_skills = (Prayer
+Faith Prowess Lvl. 1)
