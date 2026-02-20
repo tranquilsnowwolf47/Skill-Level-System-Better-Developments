82e0e415e0c03d5b3b4c83f4100e53dbf1059148
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 16:20:18 -0500

    Update AxeSkills.py

--- AxeSkills.py
+++ AxeSkills.py
@@ -1,1 +1,188 @@
-
+class AxeSkills:
+    S_plus_skills = (System Analyst 
+Project Manager 
+Master Programmer 
+Database Scientist 
+Security Essentials + 5  
+Cyber Security +5 
+C Master 
+C# Master 
+C++ Master 
+C +5
+C++ +5
+C# +5
+Troubleshooting + 5
+Loops + 5
+Arrays + 5
+Functions + 5
+Data handling + 5
+OOP + 5
+Debugging + 5
+Error Handling + 5
+)
+    S_skills = (C Mastery Lvl. 3 
+C# Mastery Lvl. 3  
+C++ Mastery Lvl. 3 
+Python +5
+Java +5
+JavaScript +5
+Loops + 4
+Arrays + 4
+Functions + 4
+Data handling + 4
+OOP + 4
+Debugging + 4
+Input Validation + 5
+Decision Structures + 5
+Error Handling + 4
+Web Development + 5
+)
+    A_plus_skills = (Python Master 
+Java Master 
+JavaScript Master 
+Database Manager  
+Programming + 5 
+Programming Logic + 5 
+Database + 5  
+Cyber Security +4  
+Python Mastery Lvl. 3 
+Java Mastery Lvl. 3 
+JavaScript Mastery Lvl. 3 
+Python +4
+Java +4
+C++ +4
+JavaScript +4
+C# +4
+C +4
+Arrays + 3
+Functions + 3
+Variables + 5
+Output + 5
+Input + 5
+Decision Structures + 4
+Web Development + 4
+Fix-it-Felix 
+Axe Prowess Lvl. 5 
+)
+    A_skills = (Programming + 4 
+Programming Logic + 4
+Database + 4
+Security Essentials + 4
+Cyber Security +3
+Python Mastery Lvl. 2 
+Java Mastery Lvl. 2
+C Mastery Lvl. 2
+C# Mastery Lvl. 2
+C++ Mastery Lvl. 2
+JavaScript Mastery Lvl. 2 
+C++ +3
+Python +3
+Java +3
+JavaScript +3
+C# +3
+C +3
+Troubleshooting + 4 
+Loops + 3
+Data handling + 3
+OOP + 3
+Variables + 4
+Input + 4
+Debugging + 3
+Input Validation + 4
+Error Handling + 3
+Handiman Lvl 3 
+)
+    B_plus_skills = (Programming + 3  
+Programming Logic + 3 
+Database + 3 
+Security Essentials + 3 
+Cyber Security +2
+Python Mastery Lvl. 1 
+Java Mastery Lvl. 1 
+C Mastery Lvl. 1 
+C# Mastery Lvl. 1 
+C++ Mastery Lvl. 1
+JavaScript Mastery Lvl. 1 
+Python +2
+Java +2
+C++ +2
+Camel Case + 3
+Snake Case + 3
+Pascal Case + 3
+Troubleshooting + 3 
+OOP + 2
+Output + 4
+Input Validation + 3
+Web Development + 3
+Axe Prowess Lvl. 4
+)
+    B_skills = (Encrypter | Requirement: Learn about encryption and how to encrypt passwords
+Programming + 2 
+Programming Logic + 2 
+Database + 2 
+Security Essentials + 2 
+Cyber Security + 1 
+C# +2
+C +2
+JavaScript +2
+Pascal Case + 2
+Troubleshooting + 2 
+Arrays + 2
+Functions + 2
+OOP + 1
+Decision Structures + 3
+)
+    C_plus_skills = (Database + 1 
+Security Essentials + 1 
+JavaScript +1
+C++ +1 
+C# +1 
+Snake Case + 2
+Loops + 2
+Functions + 1
+Data handling + 2
+Variables + 3
+Input + 3
+Decision Structures + 2
+Axe Prowess Lvl. 3
+)
+    C_skills = (Apprentice Programmer 
+Database Beginner 
+Handiman Lvl 2 
+Python + 1
+Java +1
+Camel Case + 2
+Troubleshooting + 1 
+Arrays + 1
+Output + 3
+Debugging + 2
+Input Validation + 2
+Error Handling + 2
+Web Development + 2
+)
+    D_plus_skills = (Programming Student 
+Programming Logic + 1 
+Programming + 1  
+C + 1
+Pascal Case + 1
+Data handling + 1
+Variables + 2
+Output + 2
+Debugging + 1
+Input Validation + 1
+Error Handling + 1
+)
+    D_skills = (Snake Case + 1
+Handiman Lvl 1  
+Loops + 1
+Input + 2
+Decision Structures + 1
+Axe Prowess Lvl. 2 
+)
+    E_plus_skills = (Camel Case + 1
+Variables + 1
+Output + 1
+Input + 1
+Web Development + 1
+Axe Prowess Lvl. 1
+)
