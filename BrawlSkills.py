1cb03f767099b9cc9d7edc5074b5508de0d9a235
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 16:23:27 -0500

    Update BrawlSkills.py

--- BrawlSkills.py
+++ BrawlSkills.py
@@ -1,1 +1,219 @@
-
+class BrawlSkills:
+    S_plus_skills = (Black Belt | Requirement: Get to black belt 
+Expert Marksman 
+Brown Belt 3rd Degree | Requirement: Get to 3rd degree brown belt
+Tiger Style + 5
+Crane Style + 5
+Snake Style + 5
+Leopard Style + 5
+Dragon Style + 5
+Karate Lvl. 3
+Cat Stance + 5
+Leopard Movement + 5
+Locks + 5
+Self Defense + 5
+Grappling + 5
+Throws + 5
+Parrying + 5
+Auto-Block Lvl 3
+Knife Draw Speed Lvl 3
+Gun Draw Speed Lvl 3
+Bladed Weapons + 5
+Combinations + 5
+Forms + 5
+)
+    S_skills = (Brown Belt 2nd degree | Requirement: Get to 2nd degree brown belt
+Melee Weapons Master 
+Brown Belt 1st Degree | Requirement: Get to 1st degree brown belt
+Tiger Style + 4
+Crane Style + 4
+Snake Style + 4
+Leopard Style + 4
+Dragon Style + 4
+10-Point Blocking System + 5
+Half-Moon Stance + 5
+Cat Stance + 4
+Cat Walk + 5
+Leopard Movement + 4
+Locks + 4
+Self Defense + 4
+Grappling + 4
+Throws + 4
+Parrying + 4
+Auto-Dodge Lvl 3
+Ground Defense Lvl 3
+Bladed Weapons + 4
+Combinations + 4
+Kicks Lvl. 3
+Punches Lvl. 3
+Strikes Lvl. 3
+Forms + 4
+Combat Lvl 3
+Control + 5
+Yolk + 5
+)
+    A_plus_skills = (Tiger Style + 3
+Crane Style + 3
+Snake Style + 3
+Leopard Style + 3
+Dragon Style + 3
+Karate Lvl. 2 
+10-Point Blocking System + 4
+Locks + 3
+Ground Recovery Lvl 3 
+Gun Draw Speed Lvl 2
+Bladed Weapons + 3
+Horse Stance + 5
+Grappler Lvl 3
+Brawling Prowess Lvl. 5
+)
+    A_skills = (Martial Artist 
+Blade Master
+Blunt Weapons + 3
+Defense + 3
+Marksmanship + 3
+8-Point Blocking System + 5
+10-Point Blocking System + 3
+Half-Moon Stance + 4
+Cat Stance + 3
+Cat Walk + 4
+Ground Defense Lvl 2
+Knife Draw Speed Lvl 2
+Combinations + 3
+Forms + 3
+Combat Lvl 2
+Control + 4
+Yolk + 4
+Range Gauging Lvl. 3
+)
+    B_plus_skills = (Green Belt w/ Stripe | Requirement: Get to green w/ stripe
+Non-Dominant Side Proficiency + 3
+Blunt Weapons + 2
+Dagger + 3
+Defense + 2
+Marksmanship + 2
+Tiger Style + 2
+Snake Style + 2
+Leopard Style + 2
+Dragon Style + 2
+10-Point Blocking System + 2
+Horse Stance + 4
+Self Defense + 3
+Auto-Block Lvl 2
+Ground Recovery Lvl 2
+Bladed Weapons + 2
+Brawling Prowess Lvl. 4
+)
+    B_skills = (Statue of the Crane | Requirement: Learn the completed form of Statue of the Crane from Master Dave
+Blunt Weapons + 1
+Grappler Lvl 2
+Kicks Lvl. 2
+Punches Lvl. 2
+Strikes Lvl. 2
+Dagger + 2
+Combat Lvl 1
+Defense + 1
+Non-Dominant Side Proficiency + 2
+8-Point Blocking System + 4
+10-Point Blocking System + 1
+Half-Moon Stance + 3
+Cat Stance + 2
+Cat Walk + 3
+Leopard Movement + 3
+Grappling + 3
+Throws + 3
+Parrying + 3
+Auto-Dodge Lvl 2
+Combinations + 2
+Forms + 2
+Range Gauging Lvl. 2
+Control + 3
+Yolk + 3
+)
+    C_plus_skills = (Green Belt | Requirement: Get to green belt 
+Combination 15 | Requirement: Learn the completed Combination 15 from Master Dave
+Combination 16 | Requirement: Learn the completed Combination 16 from Master Dave
+Pinion 3 | Requirement: Learn the completed form of Pinion 3 from Master Dave
+10-Point Blocking System | Requirement: Learn the 10-Point Blocking System from Master Dave 
+Kicks Lvl. 1 
+Punches Lvl. 1 
+Strikes Lvl. 1
+Dagger + 1 
+Grappler Lvl 1 
+Non-Dominant Side Proficiency + 1 
+Marksmanship + 1 
+8-Point Blocking System + 3
+Horse Stance + 3
+Cat Walk + 2
+Leopard Movement + 2
+Locks + 2
+Grappling + 2
+Throws + 2
+Parrying + 2
+Auto-Block Lvl 1
+Gun Draw Speed Lvl 1
+Bladed Weapons + 1
+Combinations + 1
+Brawling Prowess Lvl. 3
+Crane Style + 2
+Range Gauging Lvl. 1
+)
+    C_skills = (Combination 13 | Requirement: Learn the completed Combination 13 from Master Dave
+Karate Lvl. 1 
+Snake Style + 1
+Leopard Style + 1
+Dragon Style + 1
+Half-Moon Stance + 2
+Cat Stance + 1
+Auto-Dodge Lvl 1
+Ground Defense Lvl 1
+Knife Draw Speed Lvl 1
+Combinations + 1
+Control + 2
+)
+    D_plus_skills = (Blue Belt w/ Stripe | Requirement: Get to blue belt with a stripe
+Blue Belt | Requirement: Get to blue belt 
+Combination 8 | Requirement: Learn the completed Combination 8 from Master Dave
+Combination 9 | Requirement: Learn the completed Combination 9 from Master Dave
+Combination 12 | Requirement: Learn the completed Combination 12 from Master Dave
+Combination 14 | Requirement: Learn the completed Combination 14 from Master Dave
+Kata 1 | Requirement: Learn the completed form of Kata 1 from Master Dave
+Kata 2 | Requirement: Learn the completed form of Kata 2 from Master Dave
+Tiger Style + 1
+Crane Style + 1
+8-Point Blocking System + 2
+Cat Walk + 1
+Self Defense + 2
+Grappling + 1
+Throws + 1
+Parrying + 1
+Ground Recovery Lvl 1
+Horse Stance + 2
+Brawling Prowess Lvl. 2
+Control + 1
+Yolk + 2
+)
+    D_skills = (Pinion 2 | Requirement: Learn the completed form of Pinion 2 from Master Dave
+Combination 3 | Requirement: Learn the completed Combination 3 from Master Dave
+Combination 2 | Requirement: Learn the completed Combination 2 from Master Dave
+Combination 5 | Requirement: Learn the completed Combination 5 from Master Dave
+Orange Belt | Requirement: Get to orange belt 
+Combination 4 | Requirement: Learn the completed Combination 4 from Master Dave
+Combination 18 | Requirement: Learn the completed Combination 18 from Master Dave
+Purple Belt | Requirement: Get to purple belt 
+Leopard Movement + 1
+Locks + 1
+)
+    E_plus_skills = (White Belt | Requirement: Get to white belt 
+Combination 6 | Requirement: Learn the completed Combination 6 from Master Dave
+Combination 7 | Requirement: Learn the completed Combination 7 from Master Dave
+Pinion 1 | Requirement: Learn the completed form of Pinion 1 from Villari videos 
+Yellow Belt | Requirement: Get to yellow belt 
+8-Point Blocking System | Learn the 8-Point Blocking System
+8-Point Blocking System + 1
+Horse Stance + 1
+Half-Moon Stance + 1
+Self Defense + 1
+Brawling Prowess Lvl. 1
+Yolk + 1
+)
