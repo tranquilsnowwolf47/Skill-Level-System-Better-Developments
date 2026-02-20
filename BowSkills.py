355db378ade32b00f131f933056e1794bbbf414a
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 16:20:01 -0500

    Update BowSkills.py

--- BowSkills.py
+++ BowSkills.py
@@ -1,1 +1,89 @@
-
+class BowSkills:
+    S_plus_skills = (Master of All Topics 
+N3 | Requirement: Reach N3 in Japanese
+Faster Handwriting + 5
+Japanese Particles + 5
+Japanese Reading + 5
+)
+    S_skills = (Master Professor 
+Curious Learner
+Japanese Grammar + 4
+IQ + 30 
+Math + 5
+Note Taking + 5
+)
+    A_plus_skills = (Wunderkind
+N4 | Requirement: Reach N4 in Japanese
+Math + 4 
+Grammar + 3 
+Knowledge + 15  
+Neat Handwriting + 5
+Japanese Particles + 4
+Japanese Reading + 4 
+Bow Prowess Lvl. 5
+Note Taking + 4
+)
+    A_skills = (N5 | Requirement: Reach N5 in Japanese 
+Tutor
+Vocabulary + 3 
+Curiosity Loop Lvl. 3 
+ASVAB Knowledge Lvl. 3 
+IQ + 20 | B 
+Japanese Grammar +3 
+Faster Handwriting + 4
+)
+    B_plus_skills = (Nerd
+Math + 3 
+Grammar + 2  
+Knowledge + 10 
+Curiosity Loop Lvl. 2 
+Schoolwork + 3 
+ASVAB Knowledge Lvl. 2 
+Faster Handwriting + 3
+Neat Handwriting + 4
+Japanese Particles + 3
+Japanese Reading + 3
+Bow Prowess Lvl. 4
+)
+    B_skills = (First Aid 
+Japanese Language Student 
+Math + 2 
+Grammar + 1 
+Spelling + 3 
+Knowledge + 5 
+Vocabulary + 2 
+Schoolwork + 2 
+Japanese Particles + 2 
+ASVAB Knowledge Lvl. 1
+Note Taking + 3
+)
+    C_plus_skills = (Model Student 
+Math + 1
+Spelling + 2  
+Vocabulary + 1 
+IQ + 10 
+Japanese grammar + 2 
+Neat Handwriting + 3
+Japanese Particles + 1
+Bow Prowess Lvl. 3
+)
+    C_skills = (Antibody 
+Curiosity Loop Lvl. 1 
+Schoolwork + 1 
+Faster Handwriting + 2
+Japanese Reading +2
+Researcher 
+)
+    D_plus_skills = (Spelling + 1 
+Faster Handwriting + 1
+Japanese Reading + 1
+Japanese Grammar + 1 
+Bow Prowess Lvl. 2 
+Note Taking + 2
+)
+    D_skills = (Neat Handwriting + 2)
+    E_plus_skills = (Japanese Beginner
+Neat Handwriting + 1
+Bow Prowess Lvl. 1
+Note Taking + 1
+)    
