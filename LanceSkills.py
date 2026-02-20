e23daa2eb59aab3ac1819972293b1d1c2f216caf
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 16:40:53 -0500

    Update LanceSkills.py

--- LanceSkills.py
+++ LanceSkills.py
@@ -1,60 +1,60 @@
 class LanceSkills:
-    S_plus_skills = ("Pro Artist"
-"Character Design + 5"
-"Fight Scenes + 5"
-)
-    S_skills = ("Piano Master"
-"Anatomy Master"
-"Mangaka"
-"Action Poses + 5"
-)
-    A_plus_skills = ("Proportions Master"
-"Full Body + 3"
-"Fable Teller"
-"Poses + 3"
-"Clothing Design Lvl 3"
-)
-    A_skills = ("Pro Lyricist"
-"Musician"
-"Character Design + 4"
-"Anatomy + 3"
-"Full Body + 2"
-"Story Telling + 2"
-"Action Poses + 4"
-"Fight Scenes + 4"
-"Lance Prowess Lvl. 5"
-)
-    B_plus_skills = ("Art + 3"
-"Body Proportions + 3"
-"Character Design + 3"
-"Fight Scenes + 3"
-"Anatomy + 2"
-"Story Telling + 1"
-)
-    B_skills = ("Lyricist"
-"Character Design + 2"
-"Art + 2"
-"Poses + 2"
-"Body Proportions + 2"
-"Full body + 1"
-"Action Poses + 3"
-"Fight Scenes + 2"
-"Clothing Design Lvl 2"
-"Lance Prowess Lvl. 4"
-)
-    C_plus_skills = ("Character Design + 1"
-"Action Poses + 2"
-"Anatomy + 1 "
-"Fight scenes + 1"
-"Lance Prowess Lvl. 3"
-)
-    C_skills = ("Novice Musician"
-"Clothing Design Lvl 1"
-"Action Poses + 1"
-"Body Proportions + 1"
-)
-    D_plus_skills = ("Apprentice Lyricist"
-Poses + 1)
+    S_plus_skills = ("Pro Artist",
+"Character Design + 5",
+"Fight Scenes + 5")
+
+    S_skills = ("Piano Master",
+"Anatomy Master",
+"Mangaka",
+"Action Poses + 5")
+
+    A_plus_skills = ("Proportions Master",
+"Full Body + 3",
+"Fable Teller",
+"Poses + 3",
+"Clothing Design Lvl 3")
+
+    A_skills = ("Pro Lyricist",
+"Musician",
+"Character Design + 4",
+"Anatomy + 3",
+"Full Body + 2",
+"Story Telling + 2",
+"Action Poses + 4",
+"Fight Scenes + 4",
+"Lance Prowess Lvl. 5")
+
+    B_plus_skills = ("Art + 3",
+"Body Proportions + 3",
+"Character Design + 3",
+"Fight Scenes + 3",
+"Anatomy + 2",
+"Story Telling + 1")
+
+    B_skills = ("Lyricist",
+"Character Design + 2",
+"Art + 2",
+"Poses + 2",
+"Body Proportions + 2",
+"Full body + 1",
+"Action Poses + 3",
+"Fight Scenes + 2",
+"Clothing Design Lvl 2",
+"Lance Prowess Lvl. 4")
+
+    C_plus_skills = ("Character Design + 1",
+"Action Poses + 2",
+"Anatomy + 1",
+"Fight scenes + 1",
+"Lance Prowess Lvl. 3")
+
+    C_skills = ("Novice Musician",
+"Clothing Design Lvl 1",
+"Action Poses + 1",
+"Body Proportions + 1")
+
+    D_plus_skills = ("Apprentice Lyricist",
+"Poses + 1")
 
     D_skills = ("Lance Prowess Lvl. 2",
 "Art + 1")
