ae56e0a8dda7c79a07985d5e8ce0510d1bb89d46
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-21 14:14:47 -0500

    Update FlyingSkills.py

--- FlyingSkills.py
+++ FlyingSkills.py
@@ -1,1 +1,53 @@
+class FlyingSkills:
+    S_plus_skills = (
+        Cleaning Freak
+Physical Organization + 5 
+Digital Organization + 5  
+Dish Washing + 5
+Cleanliness + 5
 
+)
+    S_skills = (Always Organized 
+    Clean Mind 
+    Dish Conqueror 
+    Physical Organization + 4 
+    Digital Organization + 4
+    Sweeping + 5
+    Dish Washing + 4
+    Cleanliness + 4
+
+    )
+    A_plus_skills = (Maid 
+    Computer File Administrator 
+    Dish Washing + 3 
+    Sweeping + 4
+
+    )
+    A_skills = (Cleanliness + 3 
+    Physical Organization + 3 
+    Digital Organization + 3 
+
+    )
+    B_plus_skills = (Sweeping + 3)
+
+    B_skills = (
+        Physical Organization + 2 
+    Digital Organization + 2 
+    )
+    
+    C_plus_skills = (Cleanliness + 2 
+    )
+
+    C_skills = (Sweeping + 2 
+    Dish Washing + 2 
+    )
+
+    D_plus_skills = (Physical Organization + 1 
+    Digital Organization + 1 
+    )
+
+    D_skills = (Dish Washing + 1 
+    Cleanliness + 1 
+    )
+
+    E_plus_skills = (Sweeping + 1)
