6af46746e8a1c3e60f9f8cd3364040bd3df61f32
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-21 18:32:20 -0500

    Refactor HeavyArmorSkills class skill lists

--- HeavyArmorSkills.py
+++ HeavyArmorSkills.py
@@ -5,71 +5,71 @@
 
 class HvyArmorSkills:
     S_plus_skills = (
-"Ballet Muscles" 
-"Golem"
-"Self-control + 5"
-"Grit + 5"
-"Courage + 5"
+"Ballet Muscles", 
+"Golem",
+"Self-control + 5",
+"Grit + 5",
+"Courage + 5",
 "Discipline + 5"
 )
     
     S_skills = (
-"Defensive Tactics" 
-"Pain Tolerance + 3" 
-"Flexibility + 3" 
-"Self-control + 4"
-"Grit + 4"
-"Super Armor Lvl 3"
-"Courage + 4"
+"Defensive Tactics", 
+"Pain Tolerance + 3", 
+"Flexibility + 3", 
+"Self-control + 4",
+"Grit + 4",
+"Super Armor Lvl 3",
+"Courage + 4",
 "Discipline + 4"
 )
     
     A_plus_skills = (
-"Focused Mind"
-"Grit + 3" 
+"Focused Mind",
+"Grit + 3", 
 "Flexibility + 2" 
 )
     
     A_skills = (
-"Anti-shuffle"
-"Masochist" 
-"Resistance + 3" 
-"Self-control + 3"
-"Grit + 3"
-"Super Armor Lvl 2"
+"Anti-shuffle",
+"Masochist", 
+"Resistance + 3", 
+"Self-control + 3",
+"Grit + 3",
+"Super Armor Lvl 2",
 "Courage + 3"
 )
     
     B_plus_skills = (
-"Pain Tolerance + 2" 
+"Pain Tolerance + 2", 
 "Discipline + 3"
 )
     
     B_skills = (
-"Stubborn Fighter" 
-"Grit + 2" 
-"Self-control + 2"
-"Super Armor Lvl 1"
+"Stubborn Fighter", 
+"Grit + 2", 
+"Self-control + 2",
+"Super Armor Lvl 1",
 "Courage + 2"
 )
     
     C_plus_skills = (
-"Alert Stance" 
-"Flexibility + 1"
-"Grit + 2"
+"Alert Stance", 
+"Flexibility + 1",
+"Grit + 2",
 "Courage + 1"
 )
     
     C_skills = (
-"Resistance + 2" 
-"Self-control + 1"
+"Resistance + 2", 
+"Self-control + 1",
 "Discipline + 2"
 )
     
     D_plus_skills = ("Grit + 1")
 
     D_skills = (
-"Pain Tolerance + 1" 
+"Pain Tolerance + 1", 
 "Discipline + 1"
 )
     E_plus_skills = ("Resistance + 1")
