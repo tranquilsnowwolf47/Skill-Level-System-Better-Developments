241d3b97f41b07a4c29641059daf378102327462
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-21 18:38:38 -0500

    Create SkillManual.py with skill selection framework
    Initial creation of SkillManual.py with comments outlining functionality.

---
+++ SkillManual.py
@@ -0,0 +1,21 @@
+# Filename: SkillManual.py
+# Date: 2/21/26
+# Author: Aoi | shadowsnowowlf 
+
+
+# Let the user choose a skill category
+# Choose a letter or display all at once 
+# Allow them to view the requirement 
+
+# Will import modules 
+
+# A list that will append the unlocked skills 
+unlocked_skills = []
+
+# Ex: Dog (Has requiremnt)
+# If user wants to print out all the skills at once . do this for each
+# Maybe a nested loop
+#if input == A_plus:
+    #for skill in A_plus:
+        #print(skill)
+
