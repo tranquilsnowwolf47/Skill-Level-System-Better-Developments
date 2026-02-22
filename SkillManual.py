3680d88d8c38438caeb681886013645f2d4b0d10
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-22 17:17:25 -0500

    Update SkillManual.py

--- SkillManual.py
+++ SkillManual.py
@@ -6,6 +6,8 @@
 # Let the user choose a skill category
 # Choose a letter or display all at once 
 # Allow them to view the requirement 
+# Skill tracker program based on letter grade (proficiency level based
+#on gained experience)
 
 # Will import modules 
 
@@ -14,8 +16,10 @@ unlocked_skills = []
 
 # Ex: Dog (Has requiremnt)
 # If user wants to print out all the skills at once . do this for each
+# A function will be needed for that 
 # Maybe a nested loop
 #if input == A_plus:
     #for skill in A_plus:
         #print(skill)
 
+# Ma
