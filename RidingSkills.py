01096ea49d98340bdf478012e7e0e300bae69700
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-22 20:05:11 -0500

    Update RidingSkills.py

--- RidingSkills.py
+++ RidingSkills.py
@@ -116,65 +116,65 @@ class RidingSkills:
 # Displays the full list of E+ skills
 def display_e_plus_skills():
     print("E+:")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.E_plus_skills:
         print(skill)
 
 # Displays the full list of D skills
 def display_d_skills():
     print("D")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.D_skills:
         print(skill)
     
 # Displays the full list of D+ skills
 def display_d_plus_skills():
     print("D+")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.D_plus_skills:
         print(skill)
 
 # Displays the full list of C skills
 def display_c_skills():
     print("C")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.C_skills:
         print(skill)
 
 # Displays the full list of C+ skills
 def display_c_plus_skills():
     print("C+")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.C_plus_skills:
         print(skill)
 
 # Displays the full list of B skills
 def display_b_skills():
     print("B")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.B_skills:
         print(skill)
 
 # Displays the full list of B+ skills
 def display_b_plus_skills():
     print("B+")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.B_plus_skills:
         print(skill)
 
 # Displays the full list of A skills
 def display_a_skills():
     print("A")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.A_skills:
         print(skill)
 
 # Displays the full list of A+ skills
 def display_a_plus_skills():
     print("A+")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.A_plus_skills:
         print(skill)
 
 # Displays the full list of S skills
 def display_s_skills():
     print("S")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.S_skills:
         print(skill)
 
 # Displays the full list of S+ skills
 def display_s_plus_skills():
     print("S+")
-    for skill in RidingSkills.:
+    for skill in RidingSkills.S_plus_skills:
         print(skill)
