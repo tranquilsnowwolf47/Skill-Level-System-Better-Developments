8ef9ab66e3a7b9dc1f752d998a010efbf5166527
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-22 20:03:48 -0500

    Update LanceSkills.py

--- LanceSkills.py
+++ LanceSkills.py
@@ -84,59 +84,59 @@ def display_e_plus_skills():
 # Displays the full list of D skills
 def display_d_skills():
     print("D")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.D_skills:
         print(skill)
 
 # Displays the full list of D+ skills
 def display_d_plus_skills():
     print("D+")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.D_plus_skills:
         print(skill)
 
 # Displays the full list of C skills
 def display_c_skills():
     print("C")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.C_skills:
         print(skill)
 
 # Displays the full list of C+ skills
 def display_c_plus_skills():
     print("C+")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.C_plus_skills:
         print(skill)
 
 # Displays the full list of B skills
 def display_b_skills():
     print("B")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.B_skills:
         print(skill)
 
 # Displays the full list of B+ skills
 def display_b_plus_skills():
     print("B+")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.B_plus_skills:
         print(skill)
 
 # Displays the full list of A skills
 def display_a_skills():
     print("A")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.A_skills:
         print(skill)
 
 # Displays the full list of A+ skills
 def display_a_plus_skills():
     print("A+")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.A_plus_skills:
         print(skill)
 
 # Displays the full list of S skills
 def display_s_skills():
     print("S")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.S_skills:
         print(skill)
 
 # Displays the full list of S+ skills
 def display_s_plus_skills():
     print("S+")
-    for skill in LanceSkills.:
+    for skill in LanceSkills.S_plus_skills:
         print(skill)
