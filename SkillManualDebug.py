7799c0987d143941ef9fbe19095ec91386354235
Aoi <spadesxoathkeeper17@gmail.com>
2026-03-01 20:56:13 -0500

    Create SkillManualDebug.py

---
+++ SkillManualDebug.py
@@ -0,0 +1,60 @@
+# Filename: SkillManualDebug.py
+# Date: 2/24/26
+# Author: Aoi | shadowsnowwolf
+
+# Debugigng program for 
+
+import SwordSkills
+import LanceSkills
+import AxeSkills
+import BowSkills
+import BrawlSkills
+import ReasonSkills
+import FaithSkills
+import AuthoritySkills
+import HeavyArmorSkills
+import RidingSkills
+import FlyingSkills
+
+
+#SwordSkills.
+#LanceSkills.
+#AxeSkills.
+#BowSkills.
+#BrawlSkills.
+#ReasonSkills.
+#FaithSkills.
+#AuthoritySkills.
+#HeavyArmorSkills.
+#RidingSkills.
+#FlyingSkills.
+
+
+#SwordSkills.display_e_plus_skills()
+#SwordSkills.display_d_skills()
+#SwordSkills.display_d_plus_skills()
+#SwordSkills.display_c_skills()
+#SwordSkills.display_c_plus_skills()
+#SwordSkills.display_b_skills()
+#SwordSkills.display_b_plus_skills()
+#SwordSkills.display_a_skills()
+#SwordSkills.display_a_plus_skills()
+#SwordSkills.display_s_skills()
+#SwordSkills.display_s_plus_skills()
+#SwordSkills.display_all_sword_skills()
+
+#LanceSkills.display_e_plus_skills()
+#LanceSkills.display_d_skills()
+#LanceSkills.display_d_plus_skills()
+#LanceSkills.display_c_skills()
+#LanceSkills.display_c_plus_skills()
+#LanceSkills.display_b_skills()
+#LanceSkills.display_b_plus_skills()
+#LanceSkills.display_a_skills()
+#LanceSkills.display_a_plus_skills()
+#LanceSkills.display_s_skills()
+#LanceSkills.display_s_plus_skills()
+#LanceSkills.display_all_lance_skills()
+
+
+RidingSkills.RidingSkills.display_a_skills()
