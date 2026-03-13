b3271096f6b1e7f1d4f12bd47be758cf52b505c9
Aoi <spadesxoathkeeper17@gmail.com>
2026-03-13 11:42:43 -0400

    Update SkillManualDebug.py

--- SkillManualDebug.py
+++ SkillManualDebug.py
@@ -1,9 +1,25 @@
-# Filename: SkillManualDebug.py
-# Date: 2/24/26
-# Author: Aoi | shadowsnowwolf
+# Filename: SkillManual.py
+# Date: 2/21/26
+# Author: Aoi | shadowsnowowlf 
 
-# Debuggging program for Skill Manual
-# Test the functions to make sure everything is up to standard 
+# Let the user choose a skill category
+# Choose a letter or display all at once 
+# Allow them to view the requirement 
+# Skill tracker program based on letter grade (proficiency level based
+#on gained experience)
+
+# Will import modules 
+
+# A list that will append the unlocked skills 
+
+# Ex: Dog (Has requiremnt)
+# If user wants to print out all the skills at once . do this for each
+# A function will be needed for that 
+# Maybe a nested loop
+#if input == A_plus:
+    #for skill in A_plus:
+        #print(skill)
+
 
 import SwordSkills
 import LanceSkills
@@ -17,169 +33,64 @@ import HeavyArmorSkills
 import RidingSkills
 import FlyingSkills
 
-
-# Set true for the parameter if I want to display the skill level category (for the main program)
-
-#SwordSkills.
-#LanceSkills.
-#AxeSkills.
-#BowSkills.
-#BrawlSkills.
-#ReasonSkills.
-#FaithSkills.
-#AuthoritySkills.
-#HeavyArmorSkills.
-#RidingSkills.
-#FlyingSkills.
-
-# Sword
-#SwordSkills.display_e_plus_skills() # Debugged
-#SwordSkills.display_d_skills() # Debugged
-#SwordSkills.display_d_plus_skills()  # Debugged
-#SwordSkills.display_c_skills()  # Debugged
-#SwordSkills.display_c_plus_skills() # Debugged
-#SwordSkills.display_b_skills() # Debugged
-#SwordSkills.display_b_plus_skills() # Debugged
-#SwordSkills.display_a_skills() # Debugged
-#SwordSkills.display_a_plus_skills() # Debugged
-#SwordSkills.display_s_skills() # Debugged
-#SwordSkills.display_s_plus_skills() # Debugged  
-#SwordSkills.display_all_sword_skills() # Debugged
-
-# Lance
-#LanceSkills.display_e_plus_skills() # Debugged
-#LanceSkills.display_d_skills() # Debugged
-#LanceSkills.display_d_plus_skills()  # Debugged
-#LanceSkills.display_c_skills() # Debugged
-#LanceSkills.display_c_plus_skills() # Debugged
-#LanceSkills.display_b_skills() # Debugged
-#LanceSkills.display_b_plus_skills() # Debugged
-#LanceSkills.display_a_skills() # Debugged
-#LanceSkills.display_a_plus_skills() # Debugged
-#LanceSkills.display_s_skills() # Debugged
-#LanceSkills.display_s_plus_skills() # Debugged
-#LanceSkills.display_all_lance_skills() # Debugged
-
-# Axe
-#AxeSkills.display_e_plus_skills() # Debugged
-#AxeSkills.display_d_skills() # Debugged
-#AxeSkills.display_d_plus_skills() # Debugged
-#AxeSkills.display_c_skills() # Debugged
-#AxeSkills.display_c_plus_skills() # Debugged
-#AxeSkills.display_b_skills() # Debugged
-#AxeSkills.display_b_plus_skills() # Debugged
-#AxeSkills.display_a_skills() # Debugged
-#AxeSkills.display_a_plus_skills() # Debugged
-#AxeSkills.display_s_skills() # Debugged
-#AxeSkills.display_s_plus_skills() # Debugged
-#AxeSkills.display_all_axe_skills(True) # Debugged
-
-# Bow
-#BowSkills.display_e_plus_skills() # Debugged
-#BowSkills.display_d_skills() # Debugged
-#BowSkills.display_d_plus_skills() # Debugged
-#BowSkills.display_c_skills() # Debugged
-#BowSkills.display_c_plus_skills() # Debugged
-#BowSkills.display_b_skills() # Debugged
-#BowSkills.display_b_plus_skills() # Debugged
-#BowSkills.display_a_skills() # Debugged
-#BowSkills.display_a_plus_skills() # Debugged
-#BowSkills.display_s_skills() # Debugged
-#BowSkills.display_s_plus_skills() # Debugged
-#BowSkills.display_all_bow_skills(True) # Debugged
-
-# Brawl
-#BrawlSkills.display_e_plus_skills() # Debugged
-#BrawlSkills.display_d_skills() # Debugged
-#BrawlSkills.display_d_plus_skills() # Debugged
-#BrawlSkills.display_c_skills() # Debugged
-#BrawlSkills.display_c_plus_skills() # Debugged
-#BrawlSkills.display_b_skills() # Debugged
-#BrawlSkills.display_b_plus_skills() # Debugged
-#BrawlSkills.display_a_skills() # Debugged
-#BrawlSkills.display_a_plus_skills() # Debugged
-#BrawlSkills.display_all_brawl_skills(True) # Debugged
-
-# Reason
-#ReasonSkills.display_e_plus_skills() # Debugged
-#ReasonSkills.display_d_skills() # Debugged
-#ReasonSkills.display_d_plus_skills() # Debugged
-#ReasonSkills.display_c_skills() # Debugged
-#ReasonSkills.display_c_plus_skills() # Debugged
-#ReasonSkills.display_b_skills() # Debugged
-#ReasonSkills.display_b_plus_skills() # Debugged
-#ReasonSkills.display_a_skills() # Debugged
-#ReasonSkills.display_a_plus_skills() # Debugged
-#ReasonSkills.display_all_reason_skills() # Debugged
-
-# Faith
-#FaithSkills.display_e_plus_skills() # Debugged
-#FaithSkills.display_d_skills() # Debugged
-#FaithSkills.display_d_plus_skills() # Debugged
-#FaithSkills.display_c_skills() # Debugged
-#FaithSkills.display_c_plus_skills() # Debugged
-#FaithSkills.display_b_skills() # Debugged
-#FaithSkills.display_b_plus_skills() # Debugged
-#FaithSkills.display_a_skills() # Debugged
-#FaithSkills.display_a_plus_skills() # Debugged
-#FaithSkills.display_s_skills() # Debugged
-#FaithSkills.display_s_plus_skills() # Debugged
-#FaithSkills.display_all_faith_skills() # Debugged
-
-# Authority
-#AuthoritySkills.display_e_plus_skills() # Debugged
-#AuthoritySkills.display_d_skills() # Debugged
-#AuthoritySkills.display_d_plus_skills() # Debugged
-#AuthoritySkills.display_c_skills() # Debugged
-#AuthoritySkills.display_c_plus_skills() # Debugged
-#AuthoritySkills.display_b_skills() # Debugged
-#AuthoritySkills.display_b_plus_skills() # Debugged
-#AuthoritySkills.display_a_skills() # Debugged
-#AuthoritySkills.display_a_plus_skills() # Debugged
-#AuthoritySkills.display_s_skills() # Debugged
-#AuthoritySkills.display_s_plus_skills() # Debugged
-#AuthoritySkills.display_all_authority_skills() # Debugged
-
-# Heavy Armor 
-#HeavyArmorSkills.display_e_plus_skills() # Debugged
-#HeavyArmorSkills.display_d_skills() # Debugged
-#HeavyArmorSkills.display_d_plus_skills() # Debugged
-#HeavyArmorSkills.display_c_skills() # Debugged
-#HeavyArmorSkills.display_c_plus_skills() # Debugged
-#HeavyArmorSkills.display_b_skills() # Debugged
-#HeavyArmorSkills.display_b_plus_skills() # Debugged
-#HeavyArmorSkills.display_a_skills() # Debugged
-#HeavyArmorSkills.display_a_plus_skills() # Debugged
-#HeavyArmorSkills.display_s_skills() # Debugged
-#HeavyArmorSkills.display_s_plus_skills() # Debugged
-#HeavyArmorSkills.display_all_heavy_armor_skills() # Debugged
-
-# Riding
-#RidingSkills.display_e_plus_skills() # Debugged
-#RidingSkills.display_d_skills() # Debugged
-#RidingSkills.display_d_plus_skills() # Debugged
-#RidingSkills.display_c_skills() # Debugged 
-#RidingSkills.display_c_plus_skills() # Debugged
-#RidingSkills.display_b_skills() # Debugged
-#RidingSkills.display_b_plus_skills() # Debugged
-#RidingSkills.display_a_skills() # Debugged 
-#RidingSkills.display_a_plus_skills() # Debugged
-#RidingSkills.display_s_skills() # Debugged
-#RidingSkills.display_s_plus_skills() # Debugged
-#RidingSkills.display_all_riding_skills() # Debugged
-
-# Flying
-#FlyingSkills.display_e_plus_skills() # Debugged
-#FlyingSkills.display_d_skills() # Debugged
-#FlyingSkills.display_d_plus_skills() # Debugged
-#FlyingSkills.display_c_skills() # Debugged
-#FlyingSkills.display_c_plus_skills() # Debugged
-#FlyingSkills.display_b_skills() # Debugged
-#FlyingSkills.display_b_plus_skills() # Debugged
-#FlyingSkills.display_a_skills() # Debugged
-#FlyingSkills.display_a_plus_skills() # Debugged
-#FlyingSkills.display_s_skills() # Debugged
-#FlyingSkills.display_s_plus_skills() # Debugged
-#FlyingSkills.display_all_flying_skills() # Debugged
+skill_categories = ("Sword","Lance","Axe","Bow (Intelligence and Education)","Brawl","Reason (Mind and Spirit)","Faith","Authority","Heavy Armor","Riding (Fitness and Extracurricular)","Flying (Organizational)")
+skill_categories = (SwordSkills.skill_category, LanceSkills.skill_category, AxeSkills.skill_category, BowSkills.skill_category, BrawlSkills.skill_category,
+ReasonSkills.skill_category, FaithSkills.skill_category, AuthoritySkills.skill_category, HeavyArmorSkills.skill_category, RidingSkills.skill_category, FlyingSkills.skill_category)
 
 
+display_choices = ("All", "Letters")
+letter_grades = ("E+","D","D+","C","C+","B","B+","A","A+","S","S+")
+unlocked_skills = []
+
+# Debugging 
+# print(len(skill_categories)) 
+
+# Display the basic info
+print("Skill Categories: ")
+for skill_index, skill_category in enumerate(skill_categories,start=1):
+    print(f"\t{skill_index}. {skill_category}")
+print("---------------------------------------------------------------------")
+
+
+# Get the skill category
+skill_category_choice = input("Please enter a skill category to see info for: (1-11)").capitalize() 
+
+# Input validation
+if skill_category in skill_categories:
+    valid_skill_category = True
+    if skill_category_choice == skill_categories[0]: # 
+        print("Would you like to see all skills or by individual letter?")
+        display_choice = ("Enter 'All' for all skills or enter 'Letters' for individual letters: ")
+        if display_choice in display_choices:
+            valid_display_choice = True
+            if display_choice == display_choices[0]:
+                pass
+            elif display_choice == display_choices[1]:
+                pass
+        else:
+            valid_display_choice = False
+            print("Wrong input.")
+
+    elif skill_category_choice == skill_categories[1]:
+        pass
+    elif skill_category_choice == skill_categories[2]:
+        pass
+    elif skill_category_choice == skill_categories[3]:
+        pass
+    elif skill_category_choice == skill_categories[4]:
+        pass
+    elif skill_category_choice == skill_categories[5]:
+        pass
+    elif skill_category_choice == skill_categories[6]:
+        pass
+    elif skill_category_choice == skill_categories[7]:
+        pass
+    elif skill_category_choice == skill_categories[8]:
+        pass
+    elif skill_category_choice == skill_categories[9]:
+        pass
+    elif skill_category_choice == skill_categories[10]:
+        pass
+else:
+    valid_skill_category = False
+    print("You did not enter the correct skill category.")
