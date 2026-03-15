868ae0d9e5ea11e4cb9ed776a64cc157ae03d16a
Aoi <spadesxoathkeeper17@gmail.com>
2026-03-15 18:34:09 -0400

    Update and rename SkillManualDebug.py to SkillModulesDebug.py

---
+++ SkillModulesDebug.py
@@ -0,0 +1,185 @@
+# Filename: SkillModulesDebug.py
+# Date: 2/24/26
+# Author: Aoi | shadowsnowwolf
+
+# Debugging program for the modules for the Skill Manual Program
+# Test the functions to make sure everything is up to standard 
+
+import SwordSkills
+import LanceSkills
+import AxeSkills
+import BowSkills
+import BrawlSkills
+import ReasonSkills
+import FaithSkills
+import AuthoritySkills
+import HeavyArmorSkills
+import RidingSkills
+import FlyingSkills
+
+
+# Set true for the parameter if I want to display the skill level category (for the main program)
+
+#SwordSkills.
+#LanceSkills.
+#AxeSkills.
+#BowSkills.
+#BrawlSkills.
+#ReasonSkills.
+#FaithSkills.
+#AuthoritySkills.
+#HeavyArmorSkills.
+#RidingSkills.
+#FlyingSkills.
+
+# Sword
+#SwordSkills.display_e_plus_skills() # Debugged
+#SwordSkills.display_d_skills() # Debugged
+#SwordSkills.display_d_plus_skills()  # Debugged
+#SwordSkills.display_c_skills()  # Debugged
+#SwordSkills.display_c_plus_skills() # Debugged
+#SwordSkills.display_b_skills() # Debugged
+#SwordSkills.display_b_plus_skills() # Debugged
+#SwordSkills.display_a_skills() # Debugged
+#SwordSkills.display_a_plus_skills() # Debugged
+#SwordSkills.display_s_skills() # Debugged
+#SwordSkills.display_s_plus_skills() # Debugged  
+#SwordSkills.display_all_sword_skills() # Debugged
+
+# Lance
+#LanceSkills.display_e_plus_skills() # Debugged
+#LanceSkills.display_d_skills() # Debugged
+#LanceSkills.display_d_plus_skills()  # Debugged
+#LanceSkills.display_c_skills() # Debugged
+#LanceSkills.display_c_plus_skills() # Debugged
+#LanceSkills.display_b_skills() # Debugged
+#LanceSkills.display_b_plus_skills() # Debugged
+#LanceSkills.display_a_skills() # Debugged
+#LanceSkills.display_a_plus_skills() # Debugged
+#LanceSkills.display_s_skills() # Debugged
+#LanceSkills.display_s_plus_skills() # Debugged
+#LanceSkills.display_all_lance_skills() # Debugged
+
+# Axe
+#AxeSkills.display_e_plus_skills() # Debugged
+#AxeSkills.display_d_skills() # Debugged
+#AxeSkills.display_d_plus_skills() # Debugged
+#AxeSkills.display_c_skills() # Debugged
+#AxeSkills.display_c_plus_skills() # Debugged
+#AxeSkills.display_b_skills() # Debugged
+#AxeSkills.display_b_plus_skills() # Debugged
+#AxeSkills.display_a_skills() # Debugged
+#AxeSkills.display_a_plus_skills() # Debugged
+#AxeSkills.display_s_skills() # Debugged
+#AxeSkills.display_s_plus_skills() # Debugged
+#AxeSkills.display_all_axe_skills(True) # Debugged
+
+# Bow
+#BowSkills.display_e_plus_skills() # Debugged
+#BowSkills.display_d_skills() # Debugged
+#BowSkills.display_d_plus_skills() # Debugged
+#BowSkills.display_c_skills() # Debugged
+#BowSkills.display_c_plus_skills() # Debugged
+#BowSkills.display_b_skills() # Debugged
+#BowSkills.display_b_plus_skills() # Debugged
+#BowSkills.display_a_skills() # Debugged
+#BowSkills.display_a_plus_skills() # Debugged
+#BowSkills.display_s_skills() # Debugged
+#BowSkills.display_s_plus_skills() # Debugged
+#BowSkills.display_all_bow_skills(True) # Debugged
+
+# Brawl
+#BrawlSkills.display_e_plus_skills() # Debugged
+#BrawlSkills.display_d_skills() # Debugged
+#BrawlSkills.display_d_plus_skills() # Debugged
+#BrawlSkills.display_c_skills() # Debugged
+#BrawlSkills.display_c_plus_skills() # Debugged
+#BrawlSkills.display_b_skills() # Debugged
+#BrawlSkills.display_b_plus_skills() # Debugged
+#BrawlSkills.display_a_skills() # Debugged
+#BrawlSkills.display_a_plus_skills() # Debugged
+#BrawlSkills.display_all_brawl_skills(True) # Debugged
+
+# Reason
+#ReasonSkills.display_e_plus_skills() # Debugged
+#ReasonSkills.display_d_skills() # Debugged
+#ReasonSkills.display_d_plus_skills() # Debugged
+#ReasonSkills.display_c_skills() # Debugged
+#ReasonSkills.display_c_plus_skills() # Debugged
+#ReasonSkills.display_b_skills() # Debugged
+#ReasonSkills.display_b_plus_skills() # Debugged
+#ReasonSkills.display_a_skills() # Debugged
+#ReasonSkills.display_a_plus_skills() # Debugged
+#ReasonSkills.display_all_reason_skills() # Debugged
+
+# Faith
+#FaithSkills.display_e_plus_skills() # Debugged
+#FaithSkills.display_d_skills() # Debugged
+#FaithSkills.display_d_plus_skills() # Debugged
+#FaithSkills.display_c_skills() # Debugged
+#FaithSkills.display_c_plus_skills() # Debugged
+#FaithSkills.display_b_skills() # Debugged
+#FaithSkills.display_b_plus_skills() # Debugged
+#FaithSkills.display_a_skills() # Debugged
+#FaithSkills.display_a_plus_skills() # Debugged
+#FaithSkills.display_s_skills() # Debugged
+#FaithSkills.display_s_plus_skills() # Debugged
+#FaithSkills.display_all_faith_skills() # Debugged
+
+# Authority
+#AuthoritySkills.display_e_plus_skills() # Debugged
+#AuthoritySkills.display_d_skills() # Debugged
+#AuthoritySkills.display_d_plus_skills() # Debugged
+#AuthoritySkills.display_c_skills() # Debugged
+#AuthoritySkills.display_c_plus_skills() # Debugged
+#AuthoritySkills.display_b_skills() # Debugged
+#AuthoritySkills.display_b_plus_skills() # Debugged
+#AuthoritySkills.display_a_skills() # Debugged
+#AuthoritySkills.display_a_plus_skills() # Debugged
+#AuthoritySkills.display_s_skills() # Debugged
+#AuthoritySkills.display_s_plus_skills() # Debugged
+#AuthoritySkills.display_all_authority_skills() # Debugged
+
+# Heavy Armor 
+#HeavyArmorSkills.display_e_plus_skills() # Debugged
+#HeavyArmorSkills.display_d_skills() # Debugged
+#HeavyArmorSkills.display_d_plus_skills() # Debugged
+#HeavyArmorSkills.display_c_skills() # Debugged
+#HeavyArmorSkills.display_c_plus_skills() # Debugged
+#HeavyArmorSkills.display_b_skills() # Debugged
+#HeavyArmorSkills.display_b_plus_skills() # Debugged
+#HeavyArmorSkills.display_a_skills() # Debugged
+#HeavyArmorSkills.display_a_plus_skills() # Debugged
+#HeavyArmorSkills.display_s_skills() # Debugged
+#HeavyArmorSkills.display_s_plus_skills() # Debugged
+#HeavyArmorSkills.display_all_heavy_armor_skills() # Debugged
+
+# Riding
+#RidingSkills.display_e_plus_skills() # Debugged
+#RidingSkills.display_d_skills() # Debugged
+#RidingSkills.display_d_plus_skills() # Debugged
+#RidingSkills.display_c_skills() # Debugged 
+#RidingSkills.display_c_plus_skills() # Debugged
+#RidingSkills.display_b_skills() # Debugged
+#RidingSkills.display_b_plus_skills() # Debugged
+#RidingSkills.display_a_skills() # Debugged 
+#RidingSkills.display_a_plus_skills() # Debugged
+#RidingSkills.display_s_skills() # Debugged
+#RidingSkills.display_s_plus_skills() # Debugged
+#RidingSkills.display_all_riding_skills() # Debugged
+
+# Flying
+#FlyingSkills.display_e_plus_skills() # Debugged
+#FlyingSkills.display_d_skills() # Debugged
+#FlyingSkills.display_d_plus_skills() # Debugged
+#FlyingSkills.display_c_skills() # Debugged
+#FlyingSkills.display_c_plus_skills() # Debugged
+#FlyingSkills.display_b_skills() # Debugged
+#FlyingSkills.display_b_plus_skills() # Debugged
+#FlyingSkills.display_a_skills() # Debugged
+#FlyingSkills.display_a_plus_skills() # Debugged
+#FlyingSkills.display_s_skills() # Debugged
+#FlyingSkills.display_s_plus_skills() # Debugged
+#FlyingSkills.display_all_flying_skills() # Debugged
+
+
