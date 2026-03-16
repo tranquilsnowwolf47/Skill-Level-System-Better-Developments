a3a7d30fb58558d7058919a3c397f86a0926a1de
Aoi <spadesxoathkeeper17@gmail.com>
2026-03-15 20:48:12 -0400

    Update SkillManualDebug.py

--- SkillManualDebug.py
+++ SkillManualDebug.py
@@ -2,5 +2,14 @@
 # Date: 3/15/26
 # Author: Aoi | shadowsnowwolf 
 
+# Progrma for testing and debugging the Skill Manual 
+# program (SkillManual.py) 
+
 
 import SkillManual
+
+#print(len(SkillManual.letter_grades))
+
+# Passes the first test: (enter the skill category)
+# Passes the second test: (Display all skills)
+# third test: individual letters
