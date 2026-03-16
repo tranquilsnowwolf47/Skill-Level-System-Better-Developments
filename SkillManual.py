8d24bbd58b5640592ee6b6978561208783e0183c
Aoi <spadesxoathkeeper17@gmail.com>
2026-03-15 20:48:30 -0400

    Update SkillManual.py

--- SkillManual.py
+++ SkillManual.py
@@ -46,7 +46,7 @@ print("---------------------------------------------------------------------")
 
 
 # Get the skill category
-skill_category_choice = int(input("Please enter a skill category to see info for: (1-11)")).capitalize() 
+skill_category_choice = int(input("Please enter a skill category to see info for (1-11): "))
 
 # Input validation
 # Confirm if the user entered a valid input (number)
@@ -55,28 +55,56 @@ if skill_category_choice in skill_category_menu_choices:
 
     
     # Validate per skill category
-    # For Sword
-    if skill_category_choice == skill_category_menu_choices[0]: 
-        print("Would you like to see all skills or by individual letter?")
-        display_choice = ("Enter 'All' for all skills or enter 'Letters' for individual letters: ")
-        if display_choice in display_choices:
+    # Input validation for Sword 
+    if skill_category_choice == skill_category_menu_choices[0]:
+        # Allows the user to choose if they want to see all the skills at once, or just the skills of the correpsonding letter  
+        display_choice = input("Would you like to see all skills or by individual letter? (Enter 'All' to see all skills or 'Letters' for individual letters): ").capitalize()
+        if display_choice in display_choices: # Validate whether the user chose the right option choice or not 
             valid_display_choice = True
+    
+            # Operations for all letters
             if display_choice == display_choices[0]: # For displaying all letters
-                pass
+                header_choice = input("Would you like to see a header of the skill category? (Y or N): ").capitalize()
+                if header_choice == "Y":
+                    print()
+                    SwordSkills.display_all_sword_skills(True) # call the function that displays all skills with the header 
+                else:
+                    print()
+                    SwordSkills.display_all_sword_skills() # call the function that displays all skills without the header        
+            
+            # Operations for indivdual letters
             elif display_choice == display_choices[1]: # Display the individual letters to the user so they know which letters they can choose
                 print(f"List of {skill_categories[0]} letter grades: ")
                 for letter in letter_grades:
                     print(letter_grades)
 
-            letter_input = input("Please enter a letter to display skills for: ") # Get the letter from the user that they want to see skills for
-            if letter_input in letter_grades: # Validate whether they entered a letter in the tuple or not 
-                valid_letter = True
-                if letter_input == letter_grades[0]:
-                    pass
-                elif letter_input == letter_grades[]:
-                    pass 
-            else:
-                valid_letter = False
+                letter_input = input("Please enter a letter to display skills for: ") # Get the letter from the user that they want to see skills for
+                if letter_input in letter_grades: # Validate whether they entered a letter in the tuple or not 
+                    valid_letter = True
+                    if letter_input == letter_grades[0]:
+                        pass
+                    elif letter_input == letter_grades[1]:
+                        pass 
+                    elif letter_input == letter_grades[2]:
+                        pass 
+                    elif letter_input == letter_grades[3]:
+                        pass 
+                    elif letter_input == letter_grades[4]:
+                        pass 
+                    elif letter_input == letter_grades[5]:
+                        pass 
+                    elif letter_input == letter_grades[6]:
+                        pass 
+                    elif letter_input == letter_grades[7]:
+                        pass 
+                    elif letter_input == letter_grades[8]:
+                        pass 
+                    elif letter_input == letter_grades[9]:
+                        pass 
+                    elif letter_input == letter_grades[10]:
+                        pass 
+                else:
+                    valid_letter = False
         else:
             valid_display_choice = False
             print("Wrong input.")
@@ -101,6 +129,8 @@ if skill_category_choice in skill_category_menu_choices:
         pass
     elif skill_category_choice == skill_category_menu_choices[10]: # For Flying
         pass
+
+# Catch if the user doesn't enter the correct number corresponding to the skill category
 else:
     valid_menu_choice = False
     print("You did not enter the correct skill category.")
