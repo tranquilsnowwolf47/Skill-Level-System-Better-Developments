0fe215630df61895fd0948cb5a58993f322e5645
Aoi <spadesxoathkeeper17@gmail.com>
2026-05-03 21:01:36 -0400

    Update SkillCategoryLevelingCalculatorV1.py

--- SkillCategoryLevelingCalculatorV1.py
+++ SkillCategoryLevelingCalculatorV1.py
@@ -42,6 +42,7 @@ letter_grades = ("E","E+","D","D+","C","C+","B","B+","A","A+","S","S+","MAX")
 
 
 # I also may not need OOP. I could probably uses tuples and dictionaries
+# For the argument, I set that to the user's EXP. Which is why returning is gonna be important 
 
 class Calculator:
     def __init__(self, exp):
@@ -106,7 +107,7 @@ def calculate_E_plus(x, E_PLUS_EXP_REQUIREMENT=40):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -146,30 +147,30 @@ def calculate_D(x, D_EXP_REQUIREMENT=60):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
         print(f"\nEXP required for {new_letter}: {D_EXP_REQUIREMENT}")
 
 
-
+# Debugged
 def calculate_D_plus(x, D_PLUS_EXP_REQUIREMENT=80):
     # Requirement for promotion to letter: 80 EXP
     # Subtracts the user's total EXP points from the requirement
     # If the EXP total is greater or equal to the requirement, the user upgrades to the next letter
     
-    # Sets the current letter, granted the user didn't level up by default from the tuple (E)
-    current_letter = letter_grades[0]
+    # Sets the current letter, granted the user didn't level up by default from the tuple (D)
+    current_letter = letter_grades[2]
     
-    # Sets the upgraded letter to E+ from the tuple granted the user leveled up 
-    new_letter = letter_grades[1] 
+    # Sets the upgraded letter to D+ from the tuple granted the user leveled up 
+    new_letter = letter_grades[3] 
 
     # Test for decision branches
     # If the user leveled up, decrement the EXP and upgrade their letter grade
-    if x >= E_PLUS_EXP_REQUIREMENT:
+    if x >= D_PLUS_EXP_REQUIREMENT:
         # Perform the calculation
-        x -= E_PLUS_EXP_REQUIREMENT
+        x -= D_PLUS_EXP_REQUIREMENT
         level_up = True
 
         # Output the new info to them  
@@ -183,15 +184,29 @@ def calculate_D_plus(x, D_PLUS_EXP_REQUIREMENT=80):
     # If the user doesn't level up, just enter the EXP to next subtracted from their input
     else:
         # Calculates the deduction requirement for the next letter desired
-        EXP_to_next = E_PLUS_EXP_REQUIREMENT - x
+        EXP_to_next = D_PLUS_EXP_REQUIREMENT - x
 
         # Let the uer know that they didn't reach the EXP requirement 
-        print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print("You do not have enough EXP to reach the next letter.")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
-        print(f"\nEXP required for {new_letter}: {E_PLUS_EXP_REQUIREMENT}")
+        print(f"\nEXP required for {new_letter}: {D_PLUS_EXP_REQUIREMENT}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def calculate_C(x, C_EXP_REQUIREMENT=120):
     # Requirement for promotion to letter: 120 EXP
@@ -226,7 +241,7 @@ def calculate_C(x, C_EXP_REQUIREMENT=120):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -265,7 +280,7 @@ def calculate_C_plus(x, C_PLUS_EXP_REQUIREMENT=160):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -304,7 +319,7 @@ def calculate_B(x, B_EXP_REQUIREMENT=220):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -343,7 +358,7 @@ def calculate_B_plus(x, B_PLUS_EXP_REQUIREMENT=280):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -382,7 +397,7 @@ def calculate_A(x, A_EXP_REQUIREMENT=360):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -421,7 +436,7 @@ def calculate_A_plus(x, A_PLUS_EXP_REQUIREMENT=440):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -460,7 +475,7 @@ def calculate_S(x, S_EXP_REQUIREMENT=540):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -499,7 +514,7 @@ def calculate_S_plus(x, S_PLUS_EXP_REQUIREMENT=760):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
@@ -538,7 +553,7 @@ def calculate_MAX(x, MAX_EXP_REQUIREMENT=900):
 
         # Let the uer know that they didn't reach the EXP requirement 
         print("You do not have enough EXP to reach the next letter")
-        print(f"\nCurrent letter: {current_letter}")
+        print(f"Current letter: {current_letter}")
         # Let's the user know how much EXP they need from the current deduction to get to the next rank
         print(f"EXP to next: {EXP_to_next}")
         print("----------------------------------------------------------")
