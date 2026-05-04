06dd0804124f91576b8a94d2b670c6c9829ffcf5
Aoi <spadesxoathkeeper17@gmail.com>
2026-05-04 15:18:56 -0400

    Update SkillCategoryLevelingCalculatorV1Debug.py

--- SkillCategoryLevelingCalculatorV1Debug.py
+++ SkillCategoryLevelingCalculatorV1Debug.py
@@ -1,6 +1,6 @@
 # Debug program for SkillCategoryLevelingCalculatorV1.py
 
-import SkillCategoryLevelingCalculatorV1
+import SkillGradeProgram
 
 # Testing >=
 # Debugged
@@ -33,3 +33,69 @@ import SkillCategoryLevelingCalculatorV1
 # Testing < 
 # Debugged
 #SkillCategoryLevelingCalculatorV1.calculate_C(50)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_C_plus(160)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_C_plus(20)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_B(220)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_B(40)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_B_plus(290)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_B_plus(40)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_A(400)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_A(40)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_A_plus(500)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_A_plus(30)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_S(600)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_S(60)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_S_plus(800)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_S_plus(20)
+
+# Testing >=
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_MAX(920)
+
+# Testing < 
+# Debugged
+#SkillCategoryLevelingCalculatorV1.calculate_MAX(40)
+
+SkillGradeProgram.sword.display_skill_category()
