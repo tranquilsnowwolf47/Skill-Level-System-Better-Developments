e8b2ed10889d7351b28fe952983d1203ea19143f
Aoi <spadesxoathkeeper17@gmail.com>
2026-05-03 20:52:39 -0400

    Add test cases for SkillCategoryLevelingCalculatorV1

---
+++ SkillCategoryLevelingCalculatorV1Debug.py
@@ -0,0 +1,18 @@
+import SkillCategoryLevelingCalculatorV1
+
+# Testing >=
+# Works as intended
+#SkillCategoryLevelingCalculatorV1.calculate_E_plus(60)
+
+# Testing < 
+# Works as intended
+#SkillCategoryLevelingCalculatorV1.calculate_E_plus(20)
+
+# Testing >=
+# Works as intended
+#SkillCategoryLevelingCalculatorV1.calculate_D(120)
+
+# Testing < 
+# Works as intended
+#SkillCategoryLevelingCalculatorV1.calculate_D(40)
+
